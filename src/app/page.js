00d465ff6bf3ebcefb727392a6eab8309284e08a
bookmyassets <bookmyassets101@gmail.com>
2025-06-27 17:49:23 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -19,6 +19,10 @@ export default function Page() {
 
   return (
     <>
+    <title>Dholera: A New Era of Sustainable City Living in India</title>
+    <meta name="description" content="Invest in Dholera Smart City – a government-backed smart city with premium plots, industrial zones, and rapid infrastructure development. Secure high ROI with early investment today" />
+    <meta name="keywords" content="Dholera Smart City, Dholera plot for sale, Dholera real estate, Invest in Dholera, Residential plots in Dholera, Commercial plots in Dholera, Buy land in Dholera, Dholera investment opportunity, Dholera Smart City project, Dholera SIR plots, Dholera property prices, Dholera smart city real estate investment, Government approved plots in Dholera, Dholera international airport, Dholera expressway connectivity, Dholera industrial zone plots" />
+
       <div>
         <section>
           <LandingPage
